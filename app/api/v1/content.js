8c0b06c0c8730cae10499f18bea9eb913e40f0aa
hz-qq2 <601661706@qq.com>
2020-08-02 15:48:15 +0800

    没有加令牌和权限控制前，普通的post请求模式

--- app/api/v1/content.js
+++ app/api/v1/content.js
@@ -6,37 +6,39 @@ import { logger } from '../../middleware/logger';
 const contentApi = new LinRouter({
   prefix: '/v1/content'
 });
-contentApi.linPost(
-  'addContent',
-  '/',
-  {
-    permission: '添加期刊内容',
-    module: '内容管理',
-    mount: true
-  },
-  groupRequired,
-  logger('{user.username}新增了期刊内容'),
-  async ctx => {
-    const v = await new AddContentValidator().validate(ctx);
-    await ContentService.addContent(v.get('body'));
-    ctx.success({
-      msg: '期刊内容新增成功'
-    });
+
+contentApi.post('/', async ctx => {
+  const v = await new AddContentValidator().validate(ctx);
+  await ContentService.addContent(v.get('body'));
+  // return ctx.json(v.get('body'));
+  ctx.success({
+    msg: '期刊内容新增成功'
   });
+});
+// contentApi.linPost(
+//   'addContent',
+//   '/',
+//   {
+//     permission: '添加期刊内容',
+//     module: '内容管理',
+//     mount: true
+//   },
+//   groupRequired,
+//   logger('{user.username}新增了期刊内容'),
+//   async ctx => {
+//     const v = await new AddContentValidator().validate(ctx);
+//     await ContentService.addContent(v.get('body'));
+    // ctx.success({
+    //   msg: '期刊内容新增成功'
+    // });
+//   });
 contentApi.get('/', async ctx => {
   const contentList = await ContentService.getContentList();
   ctx.json(contentList);
 });
-contentApi.linPut(
-  'editContent',
+
+contentApi.put(
   '/:id',
-  {
-    permission: '编辑期刊内容',
-    module: '内容管理',
-    mount: true
-  },
-  groupRequired,
-  logger('{user.username}编辑了期刊内容'),
   async ctx => {
     const v = await new EditContentValidator().validate(ctx);
     const id = v.get('path.id');
@@ -46,23 +48,24 @@ contentApi.linPut(
       msg: '期刊内容编辑成功'
     });
   });
-contentApi.linDelete(
-  'delContent',
-  '/:id',
-  {
-    permission: '删除期刊内容',
-    module: '内容管理',
-    mount: true
-  },
-  groupRequired,
-  logger('{user.username}删除了期刊'),
-  async ctx => {
-    const v = await new DeleteContentValidator().validate(ctx);
-    const id = v.get('path.id');
-    const type = v.get('query.type');
-    await ContentService.deleteContent(id, type);
-    ctx.success({
-      msg: '期刊内容删除成功'
-    });
-  });
+
+// contentApi.linDelete(
+//   'delContent',
+//   '/:id',
+//   {
+//     permission: '删除期刊内容',
+//     module: '内容管理',
+//     mount: true
+//   },
+//   groupRequired,
+//   logger('{user.username}删除了期刊'),
+//   async ctx => {
+//     const v = await new DeleteContentValidator().validate(ctx);
+//     const id = v.get('path.id');
+//     const type = v.get('query.type');
+//     await ContentService.deleteContent(id, type);
+//     ctx.success({
+//       msg: '期刊内容删除成功'
+//     });
+//   });
 module.exports = { contentApi };
