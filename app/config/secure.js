c42e3fbe2b6faa43142590b5ff2ba5b3eb5cd298
hz-qq2 <601661706@qq.com>
2020-08-09 13:19:29 +0800

    后台设置好

--- app/config/secure.js
+++ app/config/secure.js
@@ -7,7 +7,7 @@ module.exports = {
     dialect: 'mysql',
     port: 3306,
     username: 'root',
-    password: 'dong1201',
+    password: 'root1111',
     logging: false,
     timezone: '+08:00',
     dialectOptions: {
--- app/service/flow.js
+++ app/service/flow.js
@@ -1,34 +1,35 @@
 import { FlowDao } from '../dao/flow';
-import {MovieModel} from '../model/movie';
-import {MusicModel} from '../model/music';
-import {SentenceModel} from '../model/sentence';
+import { MovieModel } from '../model/movie';
+import { MusicModel } from '../model/music';
+import { SentenceModel } from '../model/sentence';
 class Flow {
-    static async getFlowList() {
-        const flowList = await FlowDao.getFlowList();
-        if (flowList.length === 0) {
-            return flowList;
-        }
-        const newFlowList = [];
-        for (let i = 0; i < flowList.length; i++) {
-            let detail;
-            switch (flowList[i].type) {
-                case 100:
-                    detail = await MovieModel.findByPk(flowList[i].art_id);
-                    break;
-                case 200:
-                    detail = await MusicModel.findByPk(flowList[i].art_id);
-                    break;
-                case 300:
-                    detail = await SentenceModel.findByPk(flowList[i].art_id);
-                    break;
-
-                default:
-                    break;
-            }
-            flowList[i].setDataValue('detail', detail);
-            newFlowList.push(flowList[i]);
-        }
-        return newFlowList;
+  static async getFlowList () {
+    const flowList = await FlowDao.getFlowList();
+    if (flowList.length === 0) {
+      return flowList;
     }
+    const newFlowList = [];
+    for (let i = 0; i < flowList.length; i++) {
+      let detail;
+      switch (flowList[i].type) {
+        case 100:
+          //将flow期刊对应的movie详情给到flow接口的 detail属性上
+          detail = await MovieModel.findByPk(flowList[i].art_id);
+          break;
+        case 200:
+          detail = await MusicModel.findByPk(flowList[i].art_id);
+          break;
+        case 300:
+          detail = await SentenceModel.findByPk(flowList[i].art_id);
+          break;
+
+        default:
+          break;
+      }
+      flowList[i].setDataValue('detail', detail);
+      newFlowList.push(flowList[i]);
+    }
+    return newFlowList;
+  }
 }
 export { Flow as FlowService };
